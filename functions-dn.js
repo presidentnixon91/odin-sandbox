bb7a78a9d546a2cdc3d1c591380d2ab590b27e9e
presidentnixon91 <drewanix@yahoo.com>
2021-01-20 16:17:20 +1100

    More JS tasks

--- functions-dn.js
+++ functions-dn.js
@@ -1,5 +1,19 @@
 function random(min, max) {
-    let min = Math.ceil(min);
-    let max = Math.floor(max);
+    min = Math.ceil(min);
+    max = Math.floor(max);
     return Math.floor(Math.random() * (max - min) + min);
+}
+
+// Get user's selection
+function getUserSelection() {
+    // Get input from user
+    userSelection = prompt("Rock, Paper, Scissors?");
+    // Convert selection to sentence case
+    userSelection = userSelection.toLowerCase();
+    let firstLetter = userSelection[0];
+    firstLetter = firstLetter.toUpperCase();
+    userSelection = userSelection.slice(1);
+    userSelection = firstLetter + userSelection;
+    console.log("User selection is " + userSelection);
+    return userSelection;
 }
