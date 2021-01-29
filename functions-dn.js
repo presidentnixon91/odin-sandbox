b8e44b57114a997f4a07714f62fee5941943c61b
presidentnixon91 <drewanix@yahoo.com>
2021-01-29 19:05:10 +1100

    JS testing and Drumkit task

--- functions-dn.js
+++ functions-dn.js
@@ -17,3 +17,28 @@ function getUserSelection() {
     console.log("User selection is " + userSelection);
     return userSelection;
 }
+
+// Remove values from array
+    function removeFromArray(startingPoint) {
+        let myArray = [1, 2, 3, 4];
+
+        // Grab the additional arguments which will be removed from myArray
+        let toBeRemoved = Array.prototype.slice.call(arguments, 1);
+
+        // Go through myArray from starting point
+        for (i = startingPoint; i < myArray.length; i++) {
+            // Go through tobeRemoved array from 0
+            for (j = 0;j < toBeRemoved.length; j++) {
+                if(myArray[i] == toBeRemoved[j]) {
+                    myArray.splice(i,1);
+                }
+            }
+        }
+
+        return myArray;
+    }
+    // Send 0 as the starting point index for searching. Search for 7 and 2 to be removed from myArray
+    removeFromArray(0, 7, 2);
+
+    module.exports = removeFromArray
+
